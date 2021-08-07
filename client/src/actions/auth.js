a231b9ca6036d9683bd774ae07707b8c931ef6ab
satyansh <satyanshsinghv@gmail.com>
2021-08-07 22:31:59 +0530

    Removed git subfolder

---
+++ client/src/actions/auth.js
@@ -0,0 +1,26 @@
+import * as api from '../api/index';
+
+
+
+export const signIn = (formData, router) => async(dispatch) => {
+    try{
+        const { data } = await api.signIn(formData);
+        dispatch({ type: 'AUTH', data})
+        router.push('/');
+    }catch(error){
+        alert("Incorrect Login credentials.")
+        console.log(error);
+    }
+}
+
+
+export const signUp = (formData, router) => async(dispatch) => {
+    try{
+        const { data } = await api.signUp(formData);
+        dispatch({ type: 'AUTH', data})
+        router.push('/');
+    }catch(error){
+        console.log(error);
+        alert("Password did not match.")
+    }
+}
---
+++ client/src/api/index.js
@@ -0,0 +1,26 @@
+
+//                                          API 
+
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'https://memoriesappsatyansh.herokuapp.com' })
+
+API.interceptors.request.use((req) =>{
+    if(localStorage.getItem('profile')){
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
+
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData) => API.post('/user/signIn', formData);
+export const signUp = (formData) => API.post('/user/signUp', formData);
+
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`)
